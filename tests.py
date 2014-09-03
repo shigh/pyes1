4013a3ddb1e84c74a369f43533d6754db635f590
shigh <highscot@gmail.com>
2014-09-03 11:29:29 -0500

    Tests for move, accel and rotate

--- tests.py
+++ tests.py
@@ -39,4 +39,97 @@ class TestCalcE(unittest.TestCase):
         self.assertLess(np.abs(order-2), tol)
 
         
+class TestLeapFrog(unittest.TestCase):
+    
+    def test_two_steps(self):
+        """Two accel calls should give us one full time step
+        Using B=0
+        """
+        vx = np.linspace(0, 1, 10)
+        vy = np.zeros_like(vx)
+        E = vx.copy()
+        alpha, dt = .5, .1
+        expected = vx + alpha*E*dt
+        
+        accel(vx, vy, E, alpha, dt)
+        rotate(vx, vy, 0., dt)
+        accel(vx, vy, E, alpha, dt)
+        
+        error = np.linalg.norm(vx-expected)
+        self.assertAlmostEqual(error, 0.)
+        
+    def test_no_rotation(self):
+        """When B=wc=0 rotate should do nothing
+        """
+        vx0 = np.linspace(0, 1, 10)
+        vy0 = np.linspace(1, 2, 10)
+        vx, vy = vx0.copy(), vy0.copy()
+
+        rotate(vx, vy, 0., .1)
+
+        self.assertTrue((vx==vx0).all())
+        self.assertTrue((vy==vy0).all())
+
+    def test_half_step_back_reversible(self):
+        """The initial half step back in v should be reversible
+        """
+        vx0 = np.linspace(0, 1, 10)
+        vy0 = np.linspace(1, 2, 10)
+        vx, vy, E = vx0.copy(), vy0.copy(), vx0.copy()
+        wc, dt, alpha = .1, .1, .5
+        
+        rotate(vx, vy, -wc, dt)
+        accel(vx, vy, E, -alpha, dt)
+        accel(vx, vy, E,  alpha, dt)
+        rotate(vx, vy, wc, dt)
+        
+        self.assertAlmostEqual(np.linalg.norm(vx-vx0), 0.)
+        self.assertAlmostEqual(np.linalg.norm(vy-vy0), 0.)
+    
+    def test_normalize(self):
+        """Move particles out of [0, L) back into [0, L)
+        """
+        L = 1.
+        # The first value (-10e-20) should get washed out
+        # and rounded to 0, not to L.
+        x  = np.array([-10e-20, -1, -.5, -1e-10, 0, .5, L-1e-10, L, L+.5])
+        y  = np.array([0, 0, .5, L-1e-10, 0, .5, L-1e-10, 0, .5])
+        normalize(x, L)
+        
+        self.assertTrue((x==y).all())
+        self.assertTrue((x>=0).all())
+        self.assertTrue((x<L).all())
+    
+    def test_move(self):
+        """Everything gets pushed when do_move=None
+        """
+        L   = 1
+        dt  = .5
+        xp  = np.linspace(0, L, 20+1)[:-1]
+        vx  = np.ones_like(xp)*.2
+        vy  = None
+        xpe = xp+dt*L*.2
+        normalize(xpe, L)
+        move(xp, vx, vy, dt, L)
+
+        self.assertTrue((xp==xpe).all())
+
+    def test_move_do_move(self):
+        """Correctly moves only a subset of xp when do_move!=None
+        """
+        L   = 1
+        dt  = .5
+        do_move = np.ndarray(20, dtype=np.bool)
+        do_move[:10] = True
+        do_move[10:] = False
+        xp  = np.linspace(0, L, 20+1)[:-1]
+        vx  = np.ones_like(xp)*.2
+        vy  = None
+        xpe = xp+dt*L*.2
+        normalize(xpe, L)
+        xpe[10:] = xp[10:]
+        move(xp, vx, vy, dt, L, do_move=do_move)
+
+        self.assertTrue((xp==xpe).all())
+
         
